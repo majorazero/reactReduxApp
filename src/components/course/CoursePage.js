ff48160221e829b8f80ffa61f1c666e7312271b7
majorazero <daniel.cw.hsu@gmail.com>
2018-04-24 16:48:17 -0700

    added 'add course' mock up

--- src/components/course/CoursePage.js
+++ src/components/course/CoursePage.js
@@ -1,10 +1,38 @@
 import React, {PropTypes} from "react";
 
 class CoursePage extends React.Component {
+  constructor(props, context) { //props and context?
+    super(props, context); //you've seen this before as super();
+
+    this.state = {
+      course: {title: "" }
+    };
+
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onClickSave = this.onClickSave.bind(this);// you can do this within render, but you'll impact performance
+  }
+  onTitleChange(event) {
+    const course = this.state.course;
+    course.title = event.target.value;
+    this.setState({course: course});
+  }
+  onClickSave() {
+    alert(`Saving ${this.state.course.title}`);
+  }
   render() {
     return (
       <div>
         <h1>Courses</h1>
+        <h2>Add Course</h2>
+        <input
+          type="text"
+          onChange={this.onTitleChange}
+          value={this.state.course.title} />
+
+        <input
+          type="submit"
+          value="Save"
+          onClick={this.onClickSave} />
       </div>
     );
   }

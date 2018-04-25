c1b957ce3e4702dc52f9d49e658e695a659d8b67
majorazero <daniel.cw.hsu@gmail.com>
2018-04-24 17:40:22 -0700

    added actions constant library

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,6 +1,8 @@
+import * as types from "../actions/actionTypes";
+
 export default function courseReducer(state = [], action) { //if you pass initialState it will override your [] state
   switch(action.type) {
-    case "CREATE_COURSE":
+    case types.CREATE_COURSE:
       return [...state, //...state is a spread operator it converts it all into inline values
         Object.assign({}, action.course)];
     default:

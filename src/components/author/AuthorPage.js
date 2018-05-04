0a57bd54f7af1047d0bb32341085057d183e1586
majorazero <daniel.cw.hsu@gmail.com>
2018-05-04 14:28:18 -0700

    added an author page

---
+++ src/components/author/AuthorPage.js
@@ -0,0 +1,14 @@
+import React, {PropTypes} from "react";
+//import {connect} from "react-redux";
+import {bindActionCreators} from "redux";
+import {browserHistory} from "react-router";
+
+class AuthorPage extends React.Component {
+  render() {
+    return(
+      <h1>Author Page.</h1>
+    );
+  }
+}
+
+export default AuthorPage;
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import HomePage from "./components/home/HomePage";
 import AboutPage from "./components/about/AboutPage";
 import CoursePage from "./components/course/CoursePage";
 import ManageCoursePage from "./components/course/ManageCoursePage"; //eslint-disable-line import/no-named-as-default
+import AuthorPage from "./components/author/AuthorPage";
 
 export default (
   <Route path="/" component={App}>
@@ -13,5 +14,6 @@ export default (
     <Route path="course" component={ManageCoursePage} />
     <Route path="course/:id" component={ManageCoursePage} />
     <Route path="about" component={AboutPage} />
+    <Route path="authors" component={AuthorPage} />
   </Route>
 );
